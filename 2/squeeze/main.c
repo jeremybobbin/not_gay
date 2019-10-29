665b981c6f1c88ca7fd70f1401580bfb05787c51
Jeremy Bobbin <jer@jer.cx>
2019-10-29 05:33:03 -0700

    Not gcc's fault. I'd tried to modify a static var

--- 2/squeeze/main.c
+++ 2/squeeze/main.c
@@ -5,17 +5,12 @@ int jer_squeeze(char s1[], char s2[]) {
 
 	for (i = k = 0; s1[i] != '\0'; i++) {
 
-		int good = 1;
-
 		for (j = 0; s2[j] != '\0'; j++)
 			if (s1[i] == s2[j])
-				good = 0;
+				break;
 
-		putchar(s1[i]);
-		putchar('\n');
 
-		if (good == 1)
-			// segfaults with gcc, must be compiled wit tcc D:<
+		if (s2[j] == '\0')
 			s1[k++] = s1[i];
 
 	}
@@ -24,8 +19,8 @@ int jer_squeeze(char s1[], char s2[]) {
 }
 
 int main(int argc, char * argv) {
-	//for (int i = 1; (i + 1) < argc; i += 2) {
-	jer_squeeze("lleleeelldke", "a");
-		//printf("%s\n", argv[i]);
-	//}
+	char s[] = "lleleeelldke";
+	jer_squeeze(s, "e");
+	printf("%s\n", s);
+
 }
