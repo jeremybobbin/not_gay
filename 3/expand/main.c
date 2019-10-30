5ba51e8d9153633a9c8ed17116b9a563c5cd8723
Jeremy Bobbin <jer@jer.cx>
2019-10-30 05:01:48 -0700

    modularize

--- 3/expand/main.c
+++ 3/expand/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+int within(char c, char from, char to) {
+	return c >= from && c <= to;
+}
+
+int contiguous(char from, char to) {
+	return within(from, 'a', 'z') && within(to, 'a', 'z') ||
+		within(from, 'A', 'Z') && within(to, 'A', 'Z') ||
+		within(from, '0', '9') && within(to, '0', '9');
+}
+
 int expand(char s1[], char s2[]) {
 	int i, j;
-	char c;
+	char begin, end;
 	for (i = j = 0; s1[i] != '\0'; i++) {
-		if (s1[i + 1] == '-' && s1[i + 2] != '\0' && s1[i] < s1[i + 2]) {
-			for (c = s1[i]; c <= s1[i + 2]; c++)
-				s2[j++] = c;
-			i += 2;
-		} else
+		if (s1[i + 1] == '-' && (end = s1[i + 2]) != '\0' && contiguous(begin = s1[i], end))
+			for (i += 2; begin <= end; begin++)
+				s2[j++] = begin;
+		else
 			s2[j++] = s1[i];
 	}
 
@@ -18,7 +27,7 @@ int expand(char s1[], char s2[]) {
 
 int main(int argc, char * argv) {
 
-	char s1[] = "0-9a-zA-Z";
+	char s1[] = " -a j-l- \n0-9a-z\nA-Za-z\na-Z";
 	char s2[200] = "";
 
 	expand(s1, s2);
