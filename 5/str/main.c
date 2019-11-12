e9db51ac2254f3a75bf072fb487e6ec7b250c46c
Jeremy Bobbin <jer@jer.cx>
2019-11-12 04:55:05 -0800

    5-4 strend

--- 5/str/main.c
+++ 5/str/main.c
@@ -9,6 +9,25 @@ void strcat(char *s, char *t)
 	while (*s++ = *t++);
 }
 
+// If t occurs at end of s
+int strend(char *s, char *t)
+{
+	/* s start, t start */
+	char *ss = s;
+	char *ts = t;
+
+	while(*++s);
+	while(*++t);
+
+	while (s > ss && t > ts && *--s == *--t);
+
+	if (t == ts)
+		return 1;
+	else
+		return 0;
+
+}
+
 int main(int argc, char * argv) {
 	char s[] = "Hello, \0            ";
 	char t[] = "guy!";
@@ -17,6 +36,12 @@ int main(int argc, char * argv) {
 
 	printf("%s\n", s); // Hello, guy
 
+	if (strend(s, t))
+		printf("'%s' ends with '%s'\n", s, t);
 
+	char u[] = "I wanna fuck you in the ass Saturday.";
+	char v[] = "next wednesday instead.";
+	if (!strend(u, v))
+		printf("'%s' does not end with '%s'\n", u, v);
 
 }
