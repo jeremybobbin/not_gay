bb79e24c7d02fb13964a2be383b3e5b29c3dbcf7
Jeremy Bobbin <jer@jer.cx>
2019-11-15 06:22:48 -0800

    5-9 Rewrite day_of_year & month_day with ptrs

--- 5/days/main.c
+++ 5/days/main.c
@@ -2,44 +2,53 @@
 
 static char daytab[2][13] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	{0, 31, 29, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31},
+	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 };
 
 int day_of_year(int year, int month, int day)
 {
 	int i, leap;
+	char *cur, *max;
 
 	if (month > 12)
 		return -1;
 
 	leap = year%4 == 0 && year %100 != 0 || year%400 == 0;
-	for (i = 1; i < month; i++)
-		day += daytab[leap][i];
+
+	for (cur = daytab[leap], max = cur+month; cur < max; cur++)
+		day += *cur;
+
 	return day;
 }
 
 void month_day(int year, int yearday, int *pmonth, int *pday)
 {
 	int i, leap;
+	char *cur;
 
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 
-	if ((!leap && yearday > 365) || (leap && yearday > 364))
+	if ((!leap && yearday > 365) || (leap && yearday > 366))
 	{
 		*pmonth = -1;
 		*pday = -1;
 		return;
 	}
 
-	for (i = 1; yearday > daytab[leap][i]; i++)
-		yearday -= daytab[leap][i];
+	for (cur = &daytab[leap][1]; yearday > *cur; cur++)
+		yearday -= *cur;
 
-	*pmonth = i;
+	*pmonth = cur - daytab[leap];
 	*pday = yearday;
 }
 
 int main(int argc, char *argv) {
 	int month, day;
+
+	printf("Day of year(1998, 7, 16): %d\n\n", day_of_year(1998, 7, 16));
+	printf("Day of year(1998, 1, 1): %d\n\n", day_of_year(1998, 1, 1));
+	printf("Day of year(1998, 2, 1): %d\n\n", day_of_year(1998, 2, 1));
+
 	month_day(2001, 365, &month, &day);
 	printf("Monthday(2001, 365):\nMonth: %d\nDay: %d\n\n", month, day);
 
@@ -48,9 +57,10 @@ int main(int argc, char *argv) {
 
 
 	/* Leap years */
-	month_day(2000, 364, &month, &day);
-	printf("Monthday(2000, 364):\nMonth: %d\nDay: %d\n\n", month, day);
+	month_day(2004, 366, &month, &day);
+	printf("Monthday(2004, 366):\nMonth: %d\nDay: %d\n\n", month, day);
 
-	month_day(2000, 366, &month, &day);
-	printf("Monthday(2000, 366):\nMonth: %d\nDay: %d\n\n", month, day);
+	month_day(2004, 367, &month, &day);
+	printf("Monthday(2004, 367):\nMonth: %d\nDay: %d\n\n", month, day);
+
 }
