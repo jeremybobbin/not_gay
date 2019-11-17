27098fec62b8f4f3ba6318bb5d6f7b2d61555944
Jeremy Bobbin <jer@jer.cx>
2019-11-17 08:55:31 -0800

    5-12 add options -m, -n; tabstop every n columns

--- 5/entab/main.c
+++ 5/entab/main.c
@@ -7,27 +7,48 @@
 #define MIN(a, b) a < b ? a : b
 
 
-int parse(int *spaces, int lim, int argc, char *argv[])
+int parse(int *spaces, int lim, int argc, char *argv[], int *n, int *m)
 {
-	int i;
+	int i, j;
+	j = 0;
 	if (argc > 1)
 		for (i = 1; i < argc; i++)
-			spaces[i - 1] = atoi(argv[i]);
-	else
-		spaces[0] = DEFAULT_SPACES;
+			if (isdigit(*argv[i]))
+				spaces[j++] = atoi(argv[i]);
+			else if (*argv[i] == '-') {
+				switch(*(argv[i] + 1)) {
+					case 'm':
+						*m = atoi(argv[++i]);
+						break;
+					case 'n':
+						*n = atoi(argv[++i]);
+						break;
+					default:
+						printf("Unknown argument: %s\n", argv[i]);
+				}
+			}
 
-	return argc - 1;
+	else
+		spaces[j++] = DEFAULT_SPACES;
+
+	return j;
 }
 
 int main(int argc, char *argv[]) {
-	int c, i, j, spaces[LIST_MAX], len;
+	int c, i, j, spaces[LIST_MAX], len, n, m, col;
 	c = i = j = 0;
+	n = -1;
+	m = 0;
 
 	argc = MIN(argc, LIST_MAX);
-	len = parse(spaces, LIST_MAX, argc, argv);
+	len = parse(spaces, LIST_MAX, argc, argv, &n, &m);
 
+	col = 0;
 	while (1) {
 		c = getchar();
+		if (n != -1 && (col - m) > 0 && ((col - m) % n) == 0)
+			putchar('\t');
+		col++;
 		if (c == ' ') {
 			if (++i == spaces[j]) {
 				if (++j == len)
@@ -37,6 +58,9 @@ int main(int argc, char *argv[]) {
 			}
 			continue;
 		}
+
+		if (c == '\n')
+			col = 0;
 
 		while (i > 0) {
 			putchar(' ');
