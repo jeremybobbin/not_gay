ffe3c4887571738ccf71189869413147f58e293d
Jeremy Bobbin <jer@jer.cx>
2019-11-19 06:32:22 -0800

    numeric sort on '-n', check for -r

--- 5/sort-ptr/main.c
+++ 5/sort-ptr/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXLINES 5000
 #define MAXHEAP 5000000
@@ -61,16 +62,48 @@ int writelines(char *lineptr[], int nlines)
 		printf("%s\n", lineptr[i]);
 }
 
-void swap(char *v[], int i, int j)
+int strlen(char *s)
 {
-	char *temp;
+	char *p;
+	for (p = s; *p != '\0'; p++);
+	return p - s;
+}
+
+int reverse(char s[]) {
+	int i, j;
+	char c;
+	for (i = 0, j = (strlen(s) - 1); i < j; i++, j--) {
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
+
+int numcmp(char *s1, char *s2)
+{
+	double v1, v2;
+
+	v1 = atof(s1);
+	v2 = atof(s2);
+
+	if (v1 < v2)
+		return -1;
+	else if (v1 > v2)
+		return 1;
+	else
+		return 0;
+}
+
+void swap(void *v[], int i, int j)
+{
+	void *temp;
 
 	temp = v[i];
 	v[i] = v[j];
 	v[j] = temp;
 }
 
-void qsort(char *v[], int left, int right,
+void jsort(void *v[], int left, int right,
 		int (*comp)(void *, void *))
 {
 	int i, last;
@@ -84,16 +117,30 @@ void qsort(char *v[], int left, int right,
 			swap(v, ++last, i);
 
 	swap(v, left, last);
-	qsort(v, left, last-1, comp);
-	qsort(v, last+1, right, comp);
+	jsort(v, left, last-1, comp);
+	jsort(v, last+1, right, comp);
 }
 
-int main(int argc, char * argv) {
+int main(int argc, char *argv) {
 	int nlines;
+	int numeric, reverse;
 	char heap[MAXHEAP];
 
+	if (argc > 1 && strcmp(argv[1], "-n") == 0)
+		numeric = 1;
+
+	if (argc > 1 && strcmp(argv[1], "-r") == 0)
+		reverse = 1;
+
+	if (argc > 2 && strcmp(argv[2], "-n") == 0)
+		numeric = 1;
+
+	if (argc > 2 && strcmp(argv[2], "-r") == 0)
+		reverse = 1;
+
 	if ((nlines = readlines(lineptr, MAXLINES, heap, MAXHEAP)) >= 0) {
-		qsort(lineptr, 0, nlines-1, strcmp);
+		jsort((void **) lineptr, 0, nlines-1,
+			(int (*)(void *, void *)) (numeric ? numcmp : strcmp));
 		writelines(lineptr, nlines);
 		return 0;
 	} else {
