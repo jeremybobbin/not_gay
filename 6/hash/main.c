d6652d21f528286aceeccbafa55a661e2271cda6
Jeremy <jer@jer.cx>
2019-12-01 10:23:00 -0800

    6-6 #defininition preprocessor C:

--- 6/hash/main.c
+++ 6/hash/main.c
@@ -4,6 +4,8 @@
 #include <ctype.h>
 
 #define HASHSIZE 101
+#define MAX_WORD 50
+#define MAX_DEFN 500
 
 static struct nlist *hashtab[HASHSIZE];
 
@@ -92,13 +94,153 @@ void ungetch(int c)
 	ch = c;
 }
 
+enum { NONE = 0, STRING, CHAR_STRING, COMMENT, LINE_COMMENT, PREPROC };
+int state = 0;
+int escaped = 0;
+
+int chgood = -1;
+void ungetchgood(int c)
+{
+	chgood = c;
+}
+
+/* getch ignorignoring comments, string literals, preprocessor directives */
+#define MAX(a, b) a > b ? a : b
+
+int goodbuf = -1;
+int getchgood()
+{
+	int c, getch(void);
+	void ungetch(int);
+	if (goodbuf != -1) {
+		c = goodbuf;
+		goodbuf = -1;
+		return c;
+	}
+
+	if (chgood != -1) {
+		c = chgood;
+		chgood = -1;
+		return c;
+	}
+
+	if ((c = getch()) == EOF)
+		return c;
+
+	if (escaped > 0)
+		escaped--;
+
+	if (c == '\\')
+		if (escaped)
+			escaped = 0;
+		else
+			escaped = 2;
+
+	if (state == LINE_COMMENT || state == PREPROC) {
+		if (c == '\n' && !escaped)
+			state = NONE;
+	} else if (state == COMMENT && c == '*') {
+		if ((c = getch()) == '/') {
+			goodbuf = c;
+			return '*';
+			state = NONE;
+		}
+		else {
+			ungetch(c);
+			return '*';
+		}
+	} else if (state == STRING) {
+		if (c == '"' && !escaped)
+			state = NONE;
+	} else if (state == CHAR_STRING) {
+		if (c == '\'' && !escaped)
+			state = NONE;
+	} else if (state == NONE) {
+		if (c == '/') // "/*"
+			if ((c = getch()) == '*') {
+				goodbuf = c;
+				state = COMMENT;
+				return '/';
+			} else if (c == '/') {
+				goodbuf = c;
+				state = LINE_COMMENT;
+				return '/';
+			}
+			else {
+				ungetch(c);
+				return '/';
+			}
+		else if (c == '"')
+			state = STRING;
+		else if (c == '\'')
+			state = CHAR_STRING;
+		else if (c == '#')
+			state = PREPROC;
+	}
+
+	return c;
+}
+
+
+int getword(char *word, int lim)
+{
+	int c, getchgood(void);
+	void ungetchgood(int);
+	char *w = word;
+
+	if ((c = getchgood()) == EOF)
+		return 0;
+
+	if (isalpha(*w++ = c))
+		for ( ; --lim > 0; w++)
+			if (!(isalnum(*w = getch()) || *w == '_')) {
+				ungetchgood(*w);
+				break;
+			}
+
+	*w = '\0';
+	return w - word;
+}
+
+int instdefn() {
+	char k[MAX_WORD], v[MAX_DEFN], word[MAX_WORD];
+	v[0] = '\0';
+	int len;
+
+	getword(k, MAX_WORD); /* skip white spaces */
+	getword(k, MAX_WORD); /* skip white spaces */
+	getword(v, MAX_WORD); /* skip white spaces */
+
+	while (getword(word, MAX_WORD) != 0 && state == PREPROC) {
+		printf("%s", word);
+		strcat(v, word);
+	}
+
+	install(k, v);
+}
+
 
 int main(int argc, char *argv[])
 {
+	char c, word[MAX_WORD];
+	struct nlist *np;
+	int len;
+
 	for (int i = 0; i < HASHSIZE; i++)
 		hashtab[i] = NULL;
 
-	hashprint();
 
+
+	while ((len = getword(word, MAX_WORD)) != 0) {
+		if (len == 1)
+			putchar(word[0]);
+		else if (strcmp(word, "define") == 0 && state == PREPROC) {
+			printf("%s", word);
+			instdefn();
+		} else if (np = lookup(word)) {
+			printf("%s", np->defn);
+		} else 
+			printf("%s", word);
+	}
 	return 0;
 }
